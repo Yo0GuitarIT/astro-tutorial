38cdc815369ff9d0980fa22d4b7b687a14ee46c5
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-07-29 17:05:36 +0800

    feat: add SVG icons for theme toggle and update ThemeButton component
    - Added moon.svg and sun.svg assets for theme switching.
    - Refactored ThemeButton component to use imported SVG icons instead of inline SVG.
    - Updated BlogPost component to improve TypeScript typings.
    - Enhanced blog.astro and [tag].astro pages for better post handling.
    - Updated tsconfig.json to include types for vite-plugin-svgr.
    - Updated pnpm-lock.yaml with new dependencies for SVGR and related plugins.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import svgr from "vite-plugin-svgr";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321/",
   integrations: [react()],
+  vite: {
+    plugins: [svgr()],
+  },
 });
--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,6 +1,6 @@
 import type { Post } from "@/pages/tags/[tag].astro";
 
-function BlogPost({url, frontmatter} : Post ) {
+function BlogPost({ url, frontmatter }: Post) {
   return (
     <li>
       <a href={url}>{frontmatter.title}</a>
